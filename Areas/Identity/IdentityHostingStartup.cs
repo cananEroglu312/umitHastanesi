ebec7a0dae5ebd28f04c6356c94fa66a29654536
cananEroglu312 <64985326+cananEroglu312@users.noreply.github.com>
2022-02-09 11:42:08 +0300

    Add files via upload

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using umitHastanesiWeb.Data;
+
+[assembly: HostingStartup(typeof(umitHastanesiWeb.Areas.Identity.IdentityHostingStartup))]
+namespace umitHastanesiWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
